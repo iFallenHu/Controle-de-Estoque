599b0d488a30e314f8c7e849e7536d4c64de3fdc
Luan Reis <luannascimento.microcamp@gmail.com>
2025-04-29 21:49:22 -0300

    Created package DTO

---
+++ src/main/java/br/com/techsolucoes/ControleEstoque/DTO/CategoriaDTO.java
@@ -0,0 +1,13 @@
+package br.com.techsolucoes.ControleEstoque.DTO;
+
+public class CategoriaDTO {
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
