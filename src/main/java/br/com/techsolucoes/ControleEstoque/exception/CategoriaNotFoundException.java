3dcca6a5887c7e4de00cec094eef02ea8b702213
Luan Reis <luannascimento.microcamp@gmail.com>
2025-05-07 17:31:26 -0300

    correção da classe que trata erro

--- src/main/java/br/com/techsolucoes/ControleEstoque/exception/CategoriaNotFoundException.java
+++ src/main/java/br/com/techsolucoes/ControleEstoque/exception/CategoriaNotFoundException.java
@@ -1,8 +1,13 @@
 package br.com.techsolucoes.ControleEstoque.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class CategoriaNotFoundException extends  RuntimeException{
 
     public CategoriaNotFoundException(String messsage){
+
         super(messsage);
     }
 }
