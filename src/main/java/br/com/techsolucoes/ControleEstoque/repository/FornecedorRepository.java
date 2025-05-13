f2c7c3f6ba2d074879cb662deb27a9a55c678162
Luan Reis <luannascimento.microcamp@gmail.com>
2025-05-13 13:11:51 -0300

    Criado as classes referente a fornecedores.

---
+++ src/main/java/br/com/techsolucoes/ControleEstoque/repository/FornecedorRepository.java
@@ -0,0 +1,7 @@
+package br.com.techsolucoes.ControleEstoque.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FornecedorRepository extends JpaRepository {
+
+}
