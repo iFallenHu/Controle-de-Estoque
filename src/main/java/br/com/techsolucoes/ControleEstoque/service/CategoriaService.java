e14b3b9887e0a64647460b30ab7ff7cbb3817366
Luan Reis <luannascimento.microcamp@gmail.com>
2025-05-13 21:33:33 -0300

    identação do código

--- src/main/java/br/com/techsolucoes/ControleEstoque/service/CategoriaService.java
+++ src/main/java/br/com/techsolucoes/ControleEstoque/service/CategoriaService.java
@@ -1,58 +1,58 @@
-    package br.com.techsolucoes.ControleEstoque.service;
+package br.com.techsolucoes.ControleEstoque.service;
 
-    import br.com.techsolucoes.ControleEstoque.DTO.CategoriaDTO;
-    import br.com.techsolucoes.ControleEstoque.exception.CategoriaNotFoundException;
-    import br.com.techsolucoes.ControleEstoque.entity.Categoria;
-    import br.com.techsolucoes.ControleEstoque.repository.CategoriaRepository;
-    import org.springframework.stereotype.Service;
+import br.com.techsolucoes.ControleEstoque.DTO.CategoriaDTO;
+import br.com.techsolucoes.ControleEstoque.exception.CategoriaNotFoundException;
+import br.com.techsolucoes.ControleEstoque.entity.Categoria;
+import br.com.techsolucoes.ControleEstoque.repository.CategoriaRepository;
+import org.springframework.stereotype.Service;
 
-    import java.util.List;
+import java.util.List;
 
-    @Service
-    public class CategoriaService {
+@Service
+public class CategoriaService {
 
-        private final CategoriaRepository categoriaRepository; //referencia da instancia da interface.
+    private final CategoriaRepository categoriaRepository; //referencia da instancia da interface.
 
-        public CategoriaService(CategoriaRepository categoriaRepository) {
-            this.categoriaRepository = categoriaRepository;
-        }
-
-        public Categoria salvarCategoria(CategoriaDTO categoriaDTO) {
-            if (categoriaDTO == null){
-                throw new IllegalArgumentException("Categoria não pode ser nula");
-            }
-            Categoria categoria = new Categoria();
-            categoria.setNome(categoriaDTO.getNome());
-            return categoriaRepository.save(categoria);
-        }
-
-        public List<Categoria> listarCategoria() {
-            List<Categoria> categorias = categoriaRepository.findAll();
-            if (categorias.isEmpty()) {
-                throw new IllegalStateException("Nenhuma categoria encontrada.");
-            }
-            return categorias;
-        }
-
-        public Categoria buscarPorId(long id) {
-            return categoriaRepository.findById(id)
-                    .orElseThrow(() -> new CategoriaNotFoundException("Categoria com ID" + id + "Não encontrada"));
-        }
-
-        public void deletarCategoria(long id) {
-            Categoria categoria = categoriaRepository.findById(id)
-                    .orElseThrow(() -> new RuntimeException("Categoria não encontrada"));
-
-            categoriaRepository.deleteById(id);
-        }
-
-        public Categoria atualizarCategoria(long id, CategoriaDTO dto) {
-            Categoria categoria = categoriaRepository.findById(id)
-                    .orElseThrow(() -> new RuntimeException("Categoria não encontrada"));
-
-            categoria.setNome(dto.getNome());
-            return categoriaRepository.save(categoria);
-        }
-
-
+    public CategoriaService(CategoriaRepository categoriaRepository) {
+        this.categoriaRepository = categoriaRepository;
     }
+
+    public Categoria salvarCategoria(CategoriaDTO categoriaDTO) {
+        if (categoriaDTO == null) {
+            throw new IllegalArgumentException("Categoria não pode ser nula");
+        }
+        Categoria categoria = new Categoria();
+        categoria.setNome(categoriaDTO.getNome());
+        return categoriaRepository.save(categoria);
+    }
+
+    public List<Categoria> listarCategoria() {
+        List<Categoria> categorias = categoriaRepository.findAll();
+        if (categorias.isEmpty()) {
+            throw new IllegalStateException("Nenhuma categoria encontrada.");
+        }
+        return categorias;
+    }
+
+    public Categoria buscarPorId(long id) {
+        return categoriaRepository.findById(id)
+                .orElseThrow(() -> new CategoriaNotFoundException("Categoria com ID" + id + "Não encontrada"));
+    }
+
+    public void deletarCategoria(long id) {
+        Categoria categoria = categoriaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Categoria não encontrada"));
+
+        categoriaRepository.deleteById(id);
+    }
+
+    public Categoria atualizarCategoria(long id, CategoriaDTO dto) {
+        Categoria categoria = categoriaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Categoria não encontrada"));
+
+        categoria.setNome(dto.getNome());
+        return categoriaRepository.save(categoria);
+    }
+
+
+}
