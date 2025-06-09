04a61aa5208188837d2397f53fe7d12bed135d90
Luan Reis <luannascimento.microcamp@gmail.com>
2025-06-09 00:36:17 -0300

    adicionando condição para criar produto

--- src/main/java/br/com/techsolucoes/ControleEstoque/service/ProdutoService.java
+++ src/main/java/br/com/techsolucoes/ControleEstoque/service/ProdutoService.java
@@ -4,17 +4,18 @@ import br.com.techsolucoes.ControleEstoque.DTO.ProdutoRequestDTO;
 import br.com.techsolucoes.ControleEstoque.entity.Categoria;
 import br.com.techsolucoes.ControleEstoque.entity.Fornecedor;
 import br.com.techsolucoes.ControleEstoque.entity.Produto;
+import br.com.techsolucoes.ControleEstoque.exception.DuplicateResourceException;
 import br.com.techsolucoes.ControleEstoque.exception.ResourceNotFoundException;
 import br.com.techsolucoes.ControleEstoque.mapper.ProdutoMapper;
 import br.com.techsolucoes.ControleEstoque.repository.CategoriaRepository;
 import br.com.techsolucoes.ControleEstoque.repository.FornecedorRepository;
 import br.com.techsolucoes.ControleEstoque.repository.ProdutoRepository;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
-@AllArgsConstructor
 @RequiredArgsConstructor
 public class ProdutoService {
 
@@ -27,11 +28,19 @@ public class ProdutoService {
     private final FornecedorRepository fornecedorRepository;
 
     public Produto cadastrar(ProdutoRequestDTO produtoRequestDTO) {
+
         Categoria categoria = categoriaRepository.findById(produtoRequestDTO.getCategoriaId())
                 .orElseThrow(() -> new ResourceNotFoundException("Categoria não encontrada"));
 
         Fornecedor fornecedor = fornecedorRepository.findById(produtoRequestDTO.getFornecedorId())
                 .orElseThrow(() -> new RuntimeException("Fornecedor não encontrado"));
+
+        Optional<Produto> produtoExistente = produtoRepository.findByNomeAndCategoriaAndFornecedor(
+                produtoRequestDTO.getNome(), categoria, fornecedor);
+
+        if (produtoExistente.isPresent()) {
+            throw new DuplicateResourceException("Produto já cadastrado com esta categoria e fornecedor");
+        }
 
         Produto produto = produtoMapper.toEntity(produtoRequestDTO);
         produto.setCategoria(categoria);
