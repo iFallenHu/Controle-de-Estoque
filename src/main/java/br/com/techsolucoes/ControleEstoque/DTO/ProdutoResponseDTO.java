732903361d22f3da542f425aa2de0fa2e6c86e0f
Luan Reis <luannascimento.microcamp@gmail.com>
2025-06-09 01:03:04 -0300

    Atributos inseridos

--- src/main/java/br/com/techsolucoes/ControleEstoque/DTO/ProdutoResponseDTO.java
+++ src/main/java/br/com/techsolucoes/ControleEstoque/DTO/ProdutoResponseDTO.java
@@ -1,4 +1,17 @@
 package br.com.techsolucoes.ControleEstoque.DTO;
 
+import java.math.BigDecimal;
+
 public class ProdutoResponseDTO {
+
+    private Long id;
+    private String nome;
+    private String codigo;
+    private String descricao;
+    private String unidadeMedida;
+    private Integer estoqueMinimo = 0;
+    private Integer quantidadeAtual = 0;
+    private BigDecimal preco;
+    private Long categoriaId;
+    private Long fornecedorId;
 }
