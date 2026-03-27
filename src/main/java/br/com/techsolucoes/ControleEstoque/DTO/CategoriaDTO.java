c7c3e561f732a0dd7342eecd0a11d17c557f49aa
Luan Reis <luannascimento.microcamp@gmail.com>
2026-03-27 15:39:04 -0300

    adicionando validação no campo nome

--- src/main/java/br/com/techsolucoes/ControleEstoque/DTO/CategoriaDTO.java
+++ src/main/java/br/com/techsolucoes/ControleEstoque/DTO/CategoriaDTO.java
@@ -1,10 +1,19 @@
 package br.com.techsolucoes.ControleEstoque.DTO;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CategoriaDTO {
 
+    @NotBlank(message = "O nome não pode estar em branco")
+    @Size(min = 2, max = 100, message = "O nome deve ter entre 2 e 100 caracteres")
+    @Pattern(
+            regexp = "^[A-Za-zÀ-ÿ]+(?: [A-Za-zÀ-ÿ]+)*$",
+            message = "O nome deve conter apenas letras e espaços entre as palavras"
+    )
     private String nome;
 
 
